73201a391d23dcbe8d1d0789b0f7eb67a8bb92fd
Luis Urueta <lgurueta@misena.edu.co>
2021-08-11 18:10:43 -0500

    Avance1

---
+++ src/config/titulo.js
@@ -0,0 +1,2 @@
+module.exports =
+  'Actualización del Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST'
---
+++ src/views/Inicio.vue
@@ -0,0 +1,217 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/fondo-banner-principal.svg')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Actualización del Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST
+          
+          .h2.Txtc1 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Actualización del Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto pendiente
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
+                p.banner-principal__datos__item__texto 20 Horas
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en semanas: 
+                p.banner-principal__datos__item__texto 12 semanas
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo.mb-0 Modalidad: 
+                p.banner-principal__datos__item__texto.mb-0 virtual
+              
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p Bienvenidos al programa complementario actualización del Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST. 
+
+    p Este es un entorno de aprendizaje con el fin de prevenir, proteger y atender enfermedades y accidentes de trabajo derivados de los riesgos a los que están expuestos los trabajadores como consecuencia de las actividades diarias que desempeñan.
+
+    p El Sistema de Gestión de la Seguridad y Salud en el Trabajo (SG-SST) está basado en la normativa vigente, estos han evolucionado desde sus inicios teniendo en cuenta el componente de mejora continua. En el mundo, los sistemas de gestión están basados en el ciclo de Deming, también conocido como el ciclo PHVA, el cual consta de 4 etapas: Planear – Hacer – Verificar – Actuar.
+
+
+    .row.justify-content-center
+      .col-lg-9.mb-4-mb-lg-0
+        p La política del SG-SST de una empresa, como parte de las políticas de gestión de la misma, es un compromiso gerencial cuya finalidad es la de implementar el Sistema de Gestión de Seguridad y Salud en el Trabajo. 
+
+        p La normatividad es un mecanismo de protección para los trabajadores, por lo que los actores del SG-SST deben mantenerse actualizados para garantizar el cumplimiento de esta normatividad en las empresas y organizaciones que lideran. 
+
+        p En conclusión, el programa tiene el propósito de actualizar en todo lo concerniente a la política del Sistema de Gestión según la normativa de Seguridad y salud en el trabajo.
+
+      .col-8.col-lg-3
+        figure
+          img(src='@/assets/curso/img01.jpg', alt='SST')    
+
+    p El programa se ha diseñado a partir de contenidos temáticos y recursos educativos pensados desde:
+
+    ul.lista-ul
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Revisión de la política del SG SST. 
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Responsabilidades en el Sistema General de Riesgos Laborales.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Implementación de protocolos de bioseguridad.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Actualización del marco normativo en Seguridad y Salud en el Trabajo.
+    
+    p Una vez reconocido el contexto de aprendizaje, debe recordar que se está participando de un programa ofertado en la modalidad virtual y se deben gestionar las siguientes acciones:
+
+    ul.lista-ul
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Organizar un horario de estudio individual y colaborativo.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Revisar las notificaciones del curso constantemente.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Participar de los foros y actividades sugeridas por el instructor. 
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Entregar las evidencias esperadas durante el desarrollo de los componentes de formación.
+    
+    p.mb-0 Si tienes alguna inquietud no dude en escribirnos. Bienvenido. 
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 2. Justificación<br>del programa
+
+    p Los países miembros de la Comunidad Andina de Naciones CAN buscan promover y regular las acciones de seguridad y salud en el trabajo que se deben desarrollar en las organizaciones para disminuir o eliminar los daños a la salud del trabajador, mediante la aplicación de medidas de control y el desarrollo de las actividades necesarias para la prevención de riesgos derivados del trabajo. Colombia, siendo miembro de la CAN, avanza de manera positiva en el cumplimiento de la política pública que motiva el compromiso empresarial y social, aportando normas, requisitos y procedimientos de obligatorio cumplimiento, mediante los cuales se establece, registra, verifica y controla el funcionamiento, ejercicio y desarrollo de actividades de los empleadores y contratantes frente al Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST que promueve estilos de vida saludables y condiciones de trabajo seguras, disminuyendo los costos potenciales generados por accidentes y enfermedades laborales, y contribuyendo a la empleabilidad y seguridad social del país. 
+
+    p.mb-0 Dando cumplimiento a la normativa expedida en materia de seguridad y salud en el trabajo y a la actual situación de emergencia sanitaria generada por el coronavirus Covid-19 en todo el territorio nacional, se hace necesario que el Servicio Nacional de Aprendizaje - SENA diseñe acciones de formación para desarrollo de competencias en seguridad y salud en el trabajo, dando respuesta a los requerimientos establecidos por la Dirección de Riesgos Laborales del Ministerio de Trabajo y las necesidades de las empresas. La formación denominada Actualización del Sistema de Gestión de Seguridad y Salud en el Trabajo SG-SST busca capacitar a los responsables de la implementación del SG-SST en las organizaciones quienes deben realizar un curso mínimo cada 3 años en temas de actualización normativa, verificación y acciones de mejora los cuales son componentes fundamentales en la implementación de los SG-SST.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias<br>a desarrollar
+
+    ul.lista-ul.mb-0
+      li.d-flex.mb-0
+        i.fas.fa-angle-right
+        |	#[strong 220601073] - Controlar sistema de gestión de acuerdo con normativa de seguridad y salud en el trabajo.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 4. Perfil<br>de ingreso
+
+    ul.lista-ul.mb-0
+      li.d-flex
+        i.fas.fa-angle-right
+        |	Certificado de la formación en el Sistema de Gestión de la Seguridad y Salud en el Trabajo (50 horas).
+      li.d-flex.mb-0
+        i.fas.fa-angle-right
+        |	Cumplir con el trámite de selección definido por el centro de formación.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 5. Estrategia<br>metodológica
+
+    p Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en la utilización de las tecnologías de la información y la comunicación integradas, en ambientes virtuales de aprendizaje que, en todo caso, recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+
+    p Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    .row.g-4.justify-content-center.mb-0
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong Las TIC
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/04.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El trabajo colaborativo
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 Créditos
+
+    
+    .creditos
+      div(
+        v-for="(creditoKey, index) of Object.keys(creditosData)"
+        :class="index != Object.keys(creditosData).length -1 ? 'mb-4' : ''" 
+      )
+        .creditos__titulo {{configTitulos[creditoKey]}}
+        table
+          tbody
+            tr(
+              v-for="(item, idx) of creditosData[creditoKey]" 
+              :key="creditoKey+idx"
+            )
+              td.text-bold(colspan='2' v-html="renderText(item.nombre)")
+              td(colspan='2' v-html="renderText(item.cargo)")
+              td(colspan='3' v-html="renderText(item.centro)")
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({
+    configTitulos: {
+      liderEquipo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
+      contenidoInstruccional: 'CONTENIDO INSTRUCCIONAL',
+      desarrolloProducto:
+        'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
+      gestoresRepositorio: 'GESTORES DE REPOSITORIO',
+    },
+  }),
+  computed: {
+    creditosData() {
+      return this.$config.creditos
+    },
+  },
+  methods: {
+    renderText(textObj) {
+      let newText = ''
+      if (Array.isArray(textObj)) {
+        textObj.forEach((texto, index) => {
+          newText += (index ? '<br/>' : '') + texto
+        })
+      } else {
+        newText += textObj
+      }
+      return newText
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.creditos
+  td, th
+    border-color: $color-sistema-c
+</style>
